febbe63ee2b1f637314eeb3503a13b01bb6fbedb
Daryll <daryllghil25@gmail.com>
2024-09-26 15:09:54 +0800

    sept 26 added new component section

--- src/components/MUI X/Date Time Picker/Date/DatePicker/DesktopDatePicker/DesktopDatePicker.jsx
+++ src/components/MUI X/Date Time Picker/Date/DatePicker/DesktopDatePicker/DesktopDatePicker.jsx
@@ -3,7 +3,7 @@ import {
   LocalizationProvider,
   DesktopDatePicker as MUIDesktopDatePicker,
 } from "@mui/x-date-pickers";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import Icon from "../../../../../Foundation/Icons/Icon";
 
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -69,57 +69,51 @@ const DesktopDatePicker = ({
   );
 };
 
-// DesktopDatePicker.propTypes = {
-//   className: PropTypes.string,
-//   dayOfWeekFormatter: PropTypes.func,
-//   defaultValue: PropTypes.any,
-//   format: PropTypes.string,
-//   formatDensity: PropTypes.oneOf(["dense", "spacious"]),
-//   label: PropTypes.node,
-//   name: PropTypes.string,
-//   onChange: PropTypes.func,
-//   openTo: PropTypes.oneOf(["day", "month", "year"]),
-//   orientation: PropTypes.oneOf(["landscape", "portrait"]),
-//   referenceDate: PropTypes.any,
-//   selectedSections: PropTypes.oneOfType([
-//     PropTypes.oneOf([
-//       "all",
-//       "day",
-//       "hours",
-//       "meridiem",
-//       "minutes",
-//       "month",
-//       "seconds",
-//       "weekDay",
-//       "year",
-//     ]),
-//     PropTypes.number,
-//     PropTypes.shape({
-//       startIndex: PropTypes.number,
-//       endIndex: PropTypes.number,
-//     }),
-//   ]),
-//   slotProps: PropTypes.object,
-//   slots: PropTypes.object,
-//   sx: PropTypes.oneOfType([
-//     PropTypes.arrayOf(
-//       PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-//     ),
-//     PropTypes.func,
-//     PropTypes.object,
-//     PropTypes.bool,
-//   ]),
-//   timezone: PropTypes.string,
-//   value: PropTypes.any,
-//   view: PropTypes.oneOf(["day", "month", "year"]),
-//   views: PropTypes.arrayOf(["day", "month", "year"]),
-//   ...MUIDesktopDatePicker.propTypes,
-// };
-
-// DesktopDatePicker.defaultProps = {
-//   slots: {
-//     openPickerIcon: <Icon name="Letter" />,
-//   },
-// };
+DesktopDatePicker.propTypes = {
+  className: PropTypes.string,
+  dayOfWeekFormatter: PropTypes.func,
+  defaultValue: PropTypes.any,
+  format: PropTypes.string,
+  formatDensity: PropTypes.oneOf(["dense", "spacious"]),
+  label: PropTypes.node,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  openTo: PropTypes.oneOf(["day", "month", "year"]),
+  orientation: PropTypes.oneOf(["landscape", "portrait"]),
+  referenceDate: PropTypes.any,
+  selectedSections: PropTypes.oneOfType([
+    PropTypes.oneOf([
+      "all",
+      "day",
+      "hours",
+      "meridiem",
+      "minutes",
+      "month",
+      "seconds",
+      "weekDay",
+      "year",
+    ]),
+    PropTypes.number,
+    PropTypes.shape({
+      startIndex: PropTypes.number,
+      endIndex: PropTypes.number,
+    }),
+  ]),
+  slotProps: PropTypes.object,
+  slots: PropTypes.object,
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
+    ),
+    PropTypes.func,
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
+  timezone: PropTypes.string,
+  value: PropTypes.any,
+  view: PropTypes.oneOf(["day", "month", "year"]),
+  views: PropTypes.arrayOf(["day", "month", "year"]),
+  ...MUIDesktopDatePicker.propTypes,
+};
 
 export default DesktopDatePicker;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
-export * from "./Foundation";
 export * from "./DataDisplay";
+export * from "./Foundation";
+export * from "./MUI X";
 export * from "./Feedback";
 export * from "./Input";
+export * from "./Navigation";
+export * from "./Surfaces";
